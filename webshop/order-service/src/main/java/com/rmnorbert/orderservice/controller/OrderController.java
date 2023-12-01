15f0775a240a440441fa23fac39917e3c66502d5
Norbert Remenyi <remenyinorbert@gmail.com>
2023-12-01 17:24:09 +0100

    Feat: Implement Resilience4j in OrderController Apply @CircuitBreaker with fallback method add timeout handling and retrying on failed operations

--- webshop/order-service/src/main/java/com/rmnorbert/orderservice/controller/OrderController.java
+++ webshop/order-service/src/main/java/com/rmnorbert/orderservice/controller/OrderController.java
@@ -3,11 +3,15 @@ package com.rmnorbert.orderservice.controller;
 import com.rmnorbert.orderservice.dto.OrderRequest;
 import com.rmnorbert.orderservice.dto.OrderResponse;
 import com.rmnorbert.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,15 +19,22 @@ import java.util.List;
 public class OrderController {
     private final OrderService orderService;
 
+    // With timeout the methods executed inside a new thread
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order placed successfully";
+    @CircuitBreaker(name = "inventory" , fallbackMethod = "fallback")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<OrderResponse>  getAllOrder() {
             return orderService.getAllOrders();
+    }
+
+    public CompletableFuture<String> fallback(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Something went wrong, please order after some time!");
     }
 }
